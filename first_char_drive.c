ec0487207babaef54bbced4c1264b4f7a69c2cf4
Batuhan <batuhankertmen@sabanciuniv.edu>
2025-04-16 00:07:45 +0300

    support for static device numbers

--- first_char_drive.c
+++ first_char_drive.c
@@ -3,14 +3,32 @@
 #include <linux/fs.h>
 
 #define DEVICE_NAME "chardriver"
+#define DEVICE_COUNT 1
 
 static dev_t dev_number;
 
+static int static_major_device_num = 0;
+static int static_minor_device_num = 0;
+module_param(static_major_device_num, int, S_IRUGO);
+module_param(static_minor_device_num, int, S_IRUGO);
+
+
 static int __init initialize(void) {
-    int err = alloc_chrdev_region(&dev_number, 0, 1, DEVICE_NAME);
-    if (err) {
+    int err;
+    if (static_major_device_num){
+        dev_number = MKDEV(static_major_device_num, static_minor_device_num);
+        err = register_chrdev_region(dev_number, DEVICE_COUNT, DEVICE_NAME);
+    }
+    else {
+        err = alloc_chrdev_region(&dev_number, 0, DEVICE_COUNT, DEVICE_NAME);
+    }
+
+    if (err < 0) {
+        printk(KERN_ALERT "device number acquisation failed!");
         return err;
     }
+
+
 
     return 0;
 }
