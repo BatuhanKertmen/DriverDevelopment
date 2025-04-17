972d5d26acc55b8f39d109b78be9a18fe6aa6eb2
Batuhan <batuhankertmen@sabanciuniv.edu>
2025-04-17 13:21:38 +0000

    cdev initialization

--- first_char_drive.c
+++ first_char_drive.c
@@ -5,6 +5,7 @@
 #define DEVICE_NAME "chardriver"
 #define DEVICE_COUNT 1
 
+static struct cdev device;
 static dev_t dev_number;
 
 static int static_major_device_num = 0;
@@ -12,6 +13,9 @@ static int static_minor_device_num = 0;
 module_param(static_major_device_num, int, S_IRUGO);
 module_param(static_minor_device_num, int, S_IRUGO);
 
+static struct file_operations fops = {
+    .owner = THIS_MODULE,
+};
 
 static int __init initialize(void) {
     int err;
@@ -33,12 +37,21 @@ static int __init initialize(void) {
     static_minor_device_num = MINOR(dev_number);
     printk(KERN_INFO "Device registered: Major=%d, Minor=%d\n", static_major_device_num, static_minor_device_num);
 
+    cdev_init(&device, &fops);
+    device.owner = THIS_MODULE;
+
+    err = cdev_add(device, dev_number, DEVICE_COUNT);
+    if (err < 0) {
+        return err;
+    }
+    printk(KERN_INFO "cdev initialized");
 
     return 0;
 }
 
 static void __exit clean(void) {
     unregister_chrdev_region(dev_number, 1);
+    cdev_del(device);
 }
 
 module_init(initialize);
